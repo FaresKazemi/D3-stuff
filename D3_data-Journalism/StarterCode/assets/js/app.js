125ea39e6b6e4da3ebbadee0a8621e51a6185b4f
Fares Kazemi <Fareskazemi@live.com.au>
2022-07-12 10:30:15 +0800

    Update app.js

--- D3_data-Journalism/StarterCode/assets/js/app.js
+++ D3_data-Journalism/StarterCode/assets/js/app.js
@@ -1,4 +1,7 @@
 // @TODO: YOUR CODE HERE!
+
+
+//scatter plot
 //setting up margin
 var svgWidth = 960;
 var svgHeight = 660;
@@ -10,3 +13,21 @@ var chartMargin = {
 };
 var width = svgWidth - chartMargin.left - chartMargin.right;
 var height = svgHeight - chartMargin.top - chartMargin.bottom;
+//svg wrapper, appending svg group that will hold chart
+var svg = d3.select('#scatter')
+    .append("svg")
+    .attr("width","svgWidth")
+    .attr("height","svgHeight");
+var chartGroup = svg.append("g")
+    .attr("transform", 'translate(${margin.left}, ${margin.top})');
+
+
+//Importing Data
+d3.csv("../data/data.csv").then(function(USCensusData) {
+    //Parsing Data
+    USCensusData.foreach(function(data){
+        data.healthcare = +data.healthcare;
+        data.poverty = +data.poverty;
+        console.log(data)
+    });
+})
