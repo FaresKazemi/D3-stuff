2579479bb0827c621b1b9d7cf72c9c16b35320e2
Fareskazemi <Fareskazemi@live.com.au>
2022-07-12 08:27:24 +0800

    Update app.js

--- D3_data-Journalism/StarterCode/assets/js/app.js
+++ D3_data-Journalism/StarterCode/assets/js/app.js
@@ -1,1 +1,12 @@
 // @TODO: YOUR CODE HERE!
+//setting up margin
+var svgWidth = 960;
+var svgHeight = 660;
+var chartMargin = {
+    top:20,
+    right: 40,
+    bottom: 60,
+    left:100,
+};
+var width = svgWidth - chartMargin.left - chartMargin.right;
+var height = svgHeight - chartMargin.top - chartMargin.bottom;
